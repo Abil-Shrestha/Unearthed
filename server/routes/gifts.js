6d62361a874715a4d13f5d8fa807755419c56b51
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-10-07 14:42:02 -0400

    Week 4

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,18 +1,14 @@
 import express from 'express'
-import path from 'path'
-
-import { fileURLToPath } from 'url'
 import GiftsController from '../controllers/gifts.js'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const router = express.Router()
 
 router.get('/', GiftsController.getGifts)
+router.get('/:giftId', GiftsController.getGiftById)
 
-router.get('/:giftId', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
-})
+router.post('/', GiftsController.createGift)
 
+router.delete('/:id', GiftsController.deleteGift)
+
+router.patch('/:id', GiftsController.updateGift)
 export default router
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import giftsRouter from './routes/gifts.js'
 
 const app = express()
-
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
+app.use(cors())
+app.use(express.json())
 
 app.use('/gifts', giftsRouter)
 
